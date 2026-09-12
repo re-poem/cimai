0bb976f49ab7d6950a1f80cf39687abcbf6a529f
re-poem <bucyum@gmail.com>
2026-09-12 17:48:34 +0800

    Refactor CSharp Wrapper

--- bindings/CSharp/Cimai/Wrapper.cs
+++ bindings/CSharp/Cimai/Wrapper.cs
@@ -1,24 +1,15 @@
 using System;
-using System.ComponentModel.Design;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Cimai;
 
-/// <summary>
-/// 已解析的 Simai 文件。
-/// 持有输入文本与原生 SimaiFile 的生命周期，使用完请 Dispose()。
-/// </summary>
-public unsafe class SimaiFile : IDisposable
+public unsafe partial struct SimaiFile : IDisposable
 {
-    private readonly Native.SimaiFile _native;
-    public static SimaiFile Parse(string fumen) => new(Encoding.UTF8.GetBytes(fumen));
-    public static SimaiFile Parse(byte[] source) => new(source);
-    private SimaiFile(byte[] source)
+    public static SimaiFile Parse(string fumen) => Parse(Encoding.UTF8.GetBytes(fumen));
+    public static SimaiFile Parse(byte[] source)
     {
+        var file = stackalloc Native.SimaiFile[1];
         fixed (byte* p = source)
-        fixed (Native.SimaiFile* file = &_native)
         {
             var text = new Native.String_View
             {
@@ -26,114 +17,43 @@ public unsafe class SimaiFile : IDisposable
                 data = p,
             };
             Native.Methods.cimai_parse(&text, file);
-
-            // source还没被移走时先把数据留下来
-            Title = Helper.ReadSV(_native.title) ?? string.Empty;
-            Artist = Helper.ReadSV(_native.artist) ?? string.Empty;
-            Designer = Helper.ReadSV(_native.des) ?? string.Empty;
-            Offset = _native.offset;
-
-            var diffCount = (int)Native.SimaiDifficulty.DIFFICULTY_COUNT;
-            Charts = new SimaiChart[diffCount];
-            for (var i = 0; i < diffCount; i++)
-            {
-                Charts[i] = new(_native.charts[i]);
-            }
-            var cmdCount = _native.commands.count;
-            Commands = new SimaiCommand[cmdCount];
-            for (nuint i = 0; i < cmdCount; i++)
-            {
-                Commands[i] = new SimaiCommand(_native.commands.items[i]);
-            }
         }
+        return new SimaiFile(*file);
     }
 
     public void Dispose()
     {
-        fixed (Native.SimaiFile* file = &_native)
-            Native.Methods.cimai_file_free(file);
-        GC.SuppressFinalize(this);
+        fixed (SimaiFile* p = &this)
+        {
+            Native.Methods.cimai_file_free((Native.SimaiFile*)p);
+        }
     }
-    ~SimaiFile() => Dispose();
-
-
-
-
-    public readonly string Title;
-    public readonly string Artist;
-    public readonly string Designer;
-    public readonly float Offset;
-
-    public readonly SimaiChart[] Charts;
-    public readonly SimaiCommand[] Commands;
 }
 
-public unsafe class SimaiChart : IDisposable
+public unsafe partial struct SimaiChart : IDisposable
 {
-    private readonly Native.SimaiChart* _native;
-    private readonly bool _ownsNative;
-
-    public static SimaiChart Parse(string fumen) => new(Encoding.UTF8.GetBytes(fumen));
-    public static SimaiChart Parse(byte[] source) => new(source);
-    private SimaiChart(byte[] source)
+    public static SimaiChart Parse(string fumen) => Parse(Encoding.UTF8.GetBytes(fumen));
+    public static SimaiChart Parse(byte[] source)
     {
-        _ownsNative = true;
-
+        var chart = stackalloc Native.SimaiChart[1];
         fixed (byte* p = source)
         {
-            var text = new Native.String_View
+            chart->fumen = new Native.String_View
             {
                 count = (nuint)source.Length,
-                data = p
+                data = p,
             };
-            _native = (Native.SimaiChart*)Marshal.AllocHGlobal(sizeof(Native.SimaiChart));
-            for (int i = 0; i < sizeof(Native.SimaiChart); i++)
-                Marshal.WriteByte((IntPtr)_native, i, 0x00);
-            _native->fumen = text;
-
-            Native.Methods.cimai_parse_chart(_native);
+            Native.Methods.cimai_parse_chart(chart);
         }
-    }
-
-    internal SimaiChart(Native.SimaiChart* native)
-    {
-        _native = native;
-        _ownsNative = false;
+        return new SimaiChart(*chart);
     }
 
     public void Dispose()
     {
-        // 如果不拥有 Native 生命周期，直接跳过
-        if (!_ownsNative) return;
-
-        if (_native != null)
+        fixed (SimaiChart* self = &this)
         {
-            fixed (Native.SimaiChart** p = &_native)
-            {
-                Native.Methods.cimai_chart_free(p);
-            }
+            var p = (Native.SimaiChart*)self;
+            Native.Methods.cimai_chart_free(&p);
         }
-
-        GC.SuppressFinalize(this);
     }
-    ~SimaiChart() => Dispose();
-
-
-
-    // 请不要调用String_View content！！！！！
-    public ReadOnlySpan<SimaiTiming> Timings =>
-        new(_native->timings.items, checked((int)_native->timings.count));
 }
-
-
-public readonly struct SimaiCommand
-{
-    internal SimaiCommand(Native.SimaiCommand native)
-    {
-        Key = Helper.ReadSV(native.key) ?? string.Empty;
-        Value = Helper.ReadSV(native.value) ?? string.Empty;
-    }
-    public readonly string Key;
-    public readonly string Value;
-}
-
