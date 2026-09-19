2525757c576dd782a52c8f5f5ffdf2bb7d309f5d
re-poem <bucyum@gmail.com>
2026-09-19 12:13:09 +0800

    Remove ParseMetadata, and add DetachChart

--- bindings/CSharp/Cimai/SimaiFile.cs
+++ bindings/CSharp/Cimai/SimaiFile.cs
@@ -96,17 +96,10 @@ public sealed unsafe class SimaiFile : IDisposable
         return file;
     }
 
-    public static SimaiFile ParseMetadata(string source) => ParseMetadata(Encoding.UTF8.GetBytes(source));
-    public static SimaiFile ParseMetadata(byte[] source)
+    public void DetachChart(SimaiDifficulty difficulty)
     {
-        var file = new SimaiFile();
-        fixed (Native.SimaiFile* p = &file._native)
-        fixed (byte* sp = source)
-        {
-            var text = new Native.String_View { count = (nuint)source.Length, data = sp };
-            Native.Methods.cimai_parse_metadata(&text, p);
-        }
-        return file;
+        ThrowIfDisposed();
+        _charts[(int)difficulty] = null;
     }
 
 
